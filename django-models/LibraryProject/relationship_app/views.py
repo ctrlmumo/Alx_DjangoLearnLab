dc1c1b606785de9af3848014a5bf97b9333a42ac
Gloria <ctrlmumo11@gmail.com>
2025-11-21 12:36:26 +0300

    Adjust views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,7 @@ def list_books(request):
 # Class-based view for displaying library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
 

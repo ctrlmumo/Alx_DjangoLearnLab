ffed1c5a8f1a963298bbe6e26b173ccc3505a0e1
Gloria <ctrlmumo11@gmail.com>
2025-11-21 12:44:32 +0300

    Move html templates out of templates folder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,14 +6,14 @@ from .models import Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/templates/list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 
 # Class-based view for displaying library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/templates/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
 

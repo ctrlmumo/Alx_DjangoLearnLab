cc77abdffd87db630d3c4898b15d68b3fe4b959e
Gloria <ctrlmumo11@gmail.com>
2025-11-21 13:15:41 +0300

    Adjust relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import list_books, library_detail, login_view, logout_view, register_view
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('books/', list_books, name='list-books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-    path("register/", register_view, name="register"),
+    path('books/', views.list_books, name='list-books'),
+    path('library/<int:pk>/', views.library_detail, name='library-detail'),
+
+    # Auth required by checker
+    path('login/', 
+         LoginView.as_view(template_name='relationship_app/login.html'), 
+         name='login'),
+
+    path('logout/', 
+         LogoutView.as_view(template_name='relationship_app/logout.html'), 
+         name='logout'),
+
+    path('register/', views.register, name='register'),
 ]

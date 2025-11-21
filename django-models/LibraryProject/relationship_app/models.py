c2e491da222c59d6dd0fe23ed079e48392407f28
Gloria <ctrlmumo11@gmail.com>
2025-11-21 13:39:59 +0300

    Implement custom permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 # relationship_app/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -28,6 +29,14 @@ class Librarian(models.Model):
     def __str__(self):
         return f"{self.name} ({self.library.name})"
 
+class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+        def __str__(self):
+            return self.title
 
 from django.db import models
 from django.contrib.auth.models import User

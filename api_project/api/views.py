8333191b988fa00d86a0a1ffedfb4b238d024bb3
Gloria <ctrlmumo11@gmail.com>
2025-11-25 22:16:14 +0300

    Implemented CRUD operations with viewsets and routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from .models import Book
 from .serializers import BookSerializer
@@ -10,3 +10,7 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 # This view returns a JSON list of all books in your database using the serializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
